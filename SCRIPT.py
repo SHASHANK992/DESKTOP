d53a82e210e92c70c7088f1dc9dfddd48f2ee90c
Shashank Raina <SHASHANKRAINA@LIVE.IN>
2015-12-18 04:01:10 +0530

    SCRIPT.py

---
+++ SCRIPT.py
@@ -0,0 +1,22 @@
+
+x = input (' Enter the first value:')
+y = input ('Enter the second value:')
+x = float (x)
+y = float (y)
+
+sum = x + y
+sum = float (sum)
+diff = y-x
+if (diff < 0):
+    diff = diff * -1
+
+diff = float (diff)
+mul = x*y
+mul = float(mul)
+
+print('THE SUM OF THE VALUES IS: %f' %sum)
+
+print('THE DIFFERENCE OF THE VALUES IS: %f' %diff)
+
+print('THE MULTIPLIED VALUE OF THE VALUES IS: %f' %mul)
+
